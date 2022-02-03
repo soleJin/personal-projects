88c342cb0c4e18cd9b98a236494fa51e3cae235b
Sole <subuchappyv2@gmail.com>
2022-02-04 05:11:11 +0900

    feat: MainView - UI설정

---
+++ WeatherForecast/WeatherForecast/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  WeatherForecast
+//
+//  Created by sole on 2022/02/04.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
