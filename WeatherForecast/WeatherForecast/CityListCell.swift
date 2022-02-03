bbf46f9950c4f0930d0c2a3c18d27d757cf20f07
Sole <subuchappyv2@gmail.com>
2022-02-04 05:36:59 +0900

    feat: CityListCell 타입정의

--- WeatherForecast/WeatherForecast/CityListCell.swift
+++ WeatherForecast/WeatherForecast/CityListCell.swift
@@ -9,14 +9,25 @@ import UIKit
 
 class CityListCell: UITableViewCell {
     @IBOutlet weak var cityNameLabel: UILabel!
-    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
     @IBOutlet weak var currentHumidityLabel: UILabel!
     @IBOutlet weak var currentTemperatureLabel: UILabel!
 
     override func prepareForReuse() {
         cityNameLabel.text = nil
-        weatherIconImageView.image = nil
-        currentHumidityLabel.text = nil
         currentTemperatureLabel.text = nil
+        currentWeatherIcon.image = nil
+        currentHumidityLabel.text = nil
     }
+    
+    func update(data: CurrentWeather) {
+        cityNameLabel.text = data.cityName
+        guard let temperature = data.weather?.temperature,
+           let humidity = data.weather?.humidity,
+           let iconPath = data.additionalInformation?.first?.iconPath else { return }
+        currentWeatherIcon.image = UIImage(named: iconPath)
+        currentHumidityLabel.text = "\(humidity)"
+        currentTemperatureLabel.text = "\(temperature)"
+    }
+    
 }
---
+++ WeatherForecast/WeatherForecast/CityListViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityListViewCell.swift
+//  WeatherForecast
+//
+//  Created by sole on 2022/02/04.
+//
+
+import UIKit
+
+class CityListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
